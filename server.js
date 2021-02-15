bc0969a12aab9478c816ed225f65fee46759de58
Ubuntu <ubuntu@ip-172-31-37-38.ap-northeast-2.compute.internal>
2021-02-15 13:26:42 +0000

    다양한 환경 범용성 추가

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const process = require("process");
 const path = require("path");
 global.__ROOT_DIR = `${__dirname}/`;
 
+const secret = require(path.join(__ROOT_DIR, "secret"));
+
 const utils = require(path.join(__ROOT_DIR, "utils"));
 const logger = utils.logger("spark612 backend", (print_logger_name = false));
 
@@ -45,7 +47,7 @@ app.use(utils.request_logger(logger));
 
 // CORS
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // Frontend 주소에 맞게 수정 필요
+  res.header("Access-Control-Allow-Origin", `${secret.FRONTEND_IP}`); // Frontend 주소에 맞게 수정 필요
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, X-Access-Token"
